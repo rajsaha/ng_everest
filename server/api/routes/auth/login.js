e868358109e54566217376320c61618886d405ac
Raj Saha <rajsaha@outlook.com>
2019-05-20 22:07:56 +0800

    Password hashed during signup. Login implemented

--- server/api/routes/auth/login.js
+++ server/api/routes/auth/login.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcryptjs = require('bcryptjs');
 const User = require('../../../models/User');
 
 router.post('/', (req, res, next) => {
@@ -8,28 +9,29 @@ router.post('/', (req, res, next) => {
         password: req.body.password
     };
 
-    User.findOne({
-        username: loginCred.username,
-        password: loginCred.password
-    }).then((result) => {
-        if (result) {
-            res.status(200).json({
-                message: 'Handling POST request to /login',
-                result: result
-            });
-        } else {
-            res.status(404).json({
+    User.findOne({ username: loginCred.username }).then((data) => {
+        // If data is null
+        if (!data) {
+            return res.status(400).json({
                 message: 'User not found'
             });
         }
-    }).catch(err => {
-        console.error(err);
-        res.status(500).json({
-            error: err
+
+        // If password is invalid
+        if (!bcryptjs.compareSync(loginCred.password, data.password)) {
+            return res.status(400).json({
+                message: 'Password is invalid'
+            });
+        }
+
+        return res.status(200).json({
+            message: 'Logged in successfully'
+        });        
+    }).catch((err) => {
+        return res.status(404).json({
+            error: err.message
         });
     });
-
-
 });
 
 module.exports = router;
--- server/api/routes/auth/signup.js
+++ server/api/routes/auth/signup.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');
 const Validation = require('../../../services/validation/validation');
 const User = require('../../../models/User');
 
@@ -8,11 +9,12 @@ router.post('/', (req, res, next) => {
     const signup_validation_result = Validation.SignUpDataValidation(req.body.email, req.body.username, req.body.password);
     signup_validation_result.then((response) => {
         if (response.status) {
+            const hashedPass = bcryptjs.hashSync(req.body.password, 10);
             const user = new User({
                 _id: new mongoose.Types.ObjectId(),
                 email: req.body.email,
                 username: req.body.username,
-                password: req.body.password
+                password: hashedPass
             });
             user.save()
                 .then(result => {
@@ -22,9 +24,8 @@ router.post('/', (req, res, next) => {
                     });
                 })
                 .catch(err => {
-                    console.error(err);
                     res.status(500).json({
-                        error: err
+                        error: err.message
                     });
                 });
         } else {
