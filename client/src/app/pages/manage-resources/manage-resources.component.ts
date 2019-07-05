8f96ab648dd640dca28b9a0b2d1444f45e9e911a
Raj Saha <saha.raj@mobileone.com.my>
2019-07-05 16:43:12 +0800

    wip manage resources

--- client/src/app/pages/manage-resources/manage-resources.component.ts
+++ client/src/app/pages/manage-resources/manage-resources.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { faSearch, faEye, faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { ResourceService } from '@services/resource/resource.service';
 
 @Component({
   selector: 'app-manage-resources',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageResourcesComponent implements OnInit {
 
-  constructor() { }
+  // Icons
+  faSearch = faSearch;
+  faEye = faEye;
+  faEdit = faEdit;
+  faTrashAlt = faTrashAlt;
+
+  searchForm: FormGroup;
+  username: any;
+  posts: any;
+
+  constructor(private fb: FormBuilder, private resourceService: ResourceService) { }
 
   ngOnInit() {
+    this.username = localStorage.getItem('username');
+    this.initSearchForm();
+    this.getAllResources();
+  }
+
+  initSearchForm() {
+    this.searchForm = this.fb.group({
+      query: ['']
+    });
+  }
+
+  async getAllResources() {
+    try {
+      const response = await this.resourceService.getAllResources({username: this.username});
+      this.posts = response.resources;
+    } catch (err) {
+      console.error(err);
+    }
   }
 
 }
