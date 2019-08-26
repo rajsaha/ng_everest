61b87f386201d06b7fc79c7218a7974641ddab61
Raj Saha <rajsaha@outlook.com>
2019-08-27 00:03:42 +0800

    wip search

--- src/app/layouts/main/main.component.ts
+++ src/app/layouts/main/main.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectorRef, Component, OnInit, OnDestroy } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { Router } from '@angular/router';
-import { faUser, faSignOutAlt, faStream, faShareAlt, faBorderAll } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faSignOutAlt, faStream, faShareAlt, faBorderAll, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { faSquare } from '@fortawesome/free-regular-svg-icons';
 import { LoginService } from '@services/auth/login.service';
 import { CommunicationService } from '@services/general/communication.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
@@ -30,6 +31,12 @@ export class MainComponent implements OnInit, OnDestroy {
   faShareAlt = faShareAlt;
   faSquare = faSquare;
   faBorderAll = faBorderAll;
+  faSearch = faSearch;
+
+  // Search Form
+  searchForm = this.fb.group({
+    query: ['']
+  });
 
   private mobileQueryListener: () => void;
 
@@ -38,7 +45,8 @@ export class MainComponent implements OnInit, OnDestroy {
     media: MediaMatcher,
     private router: Router,
     private loginService: LoginService,
-    private communicationService: CommunicationService) {
+    private communicationService: CommunicationService,
+    private fb: FormBuilder) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this.mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this.mobileQueryListener);
