4fabbd40e7f99dbbf03592eabf61b306974401e9
Raj Saha <saha.raj@mobileone.com.my>
2019-09-13 16:18:51 +0800

    added site url for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'https://ng-everest.herokuapp.com'
+  API_URL: 'https://ng-everest.herokuapp.com',
+  SITE_URL: 'https://ng-everest-client.herokuapp.com'
 };
