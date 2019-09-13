8c5f4dc46b8e95da63b221c90549b11c8445da67
Raj Saha <saha.raj@mobileone.com.my>
2019-09-13 18:12:35 +0800

    enabled prod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, enableProdMode } from '@angular/core';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptorService } from '@services/auth/auth-interceptor.service';
 
@@ -72,4 +72,5 @@ import { SearchComponent } from './components/search/search.component';
 })
 export class AppModule { }
 
+enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
