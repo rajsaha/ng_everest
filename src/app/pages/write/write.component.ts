59264771e9371a7b227bc55e522f93b990b7151d
Raj Saha <saha.raj@mobileone.com.my>
2019-09-23 17:23:20 +0800

    wip write page

--- src/app/pages/write/write.component.ts
+++ src/app/pages/write/write.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import * as BalloonEditor from '@ckeditor/ckeditor5-build-balloon';
+import { Article } from 'src/app/interfaces/article';
+import { CloudinaryImageUploadAdapter } from 'ckeditor-cloudinary-uploader-adapter';
 
 @Component({
   selector: 'app-write',
@@ -7,12 +9,37 @@ import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
   styleUrls: ['./write.component.scss']
 })
 export class WriteComponent implements OnInit {
-  // Editor
-  public Editor = ClassicEditor;
+  article: Article;
 
-  constructor() { }
+  // Editor
+  public Editor = BalloonEditor;
+  editorConfig = {
+    placeholder: 'Share your thoughts...',
+    extraPlugins: [ this.imagePluginFactory ]
+  };
+
+  constructor() {
+    this.article = {
+      title: '',
+      text: ''
+    };
+  }
 
   ngOnInit() {
+  }
+
+  imagePluginFactory(editor) {
+    try {
+      editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+        return new CloudinaryImageUploadAdapter(
+          loader,
+          'rajsaha',
+          'ebgizjgk',
+          [ 160, 500, 1000, 1052 ]);
+      };
+    } catch (error) {
+      console.error(error);
+    }
   }
 
 }
