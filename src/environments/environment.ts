6dbb9dbd7f7c6d1e3bcf56f0faebc1d7d385e0ac
Raj Saha <saha.raj@mobileone.com.my>
2019-11-06 18:38:46 +0800

    fixes for deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:4000',
-  SITE_URL: 'http://localhost:4444',
+  API_URL: 'https://ng-everest.herokuapp.com',
+  SITE_URL: 'https://ng-everest-client.herokuapp.com',
   TINYMCE_API_KEY: 'lqpwv4p648mq5mgz5h2yl8auqyr0j6oqtskmeiyerhz8aype'
 };
 
