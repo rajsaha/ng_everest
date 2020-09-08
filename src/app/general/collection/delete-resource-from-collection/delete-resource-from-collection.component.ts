bdc2f74a9a97d38606814df7f39059665e3e08c2
Raj Saha <saha.raj@mobileone.com.my>
2020-09-08 18:43:37 +0800

    working on delete resource from collection function

--- src/app/general/collection/delete-resource-from-collection/delete-resource-from-collection.component.ts
+++ src/app/general/collection/delete-resource-from-collection/delete-resource-from-collection.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import { CollectionService } from '@services/collection/collection.service';
 
 @Component({
   selector: 'app-delete-resource-from-collection',
@@ -8,15 +9,26 @@ import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 })
 export class DeleteResourceFromCollectionComponent implements OnInit {
   @Input() data: any;
+  @Input() collectionId: any;
   isActive = false;
   // Icons
   faTimesCircle = faTimesCircle;
-  constructor() { }
+  constructor(private collectionService: CollectionService) { }
 
   ngOnInit() {
+    console.log(this.data)
   }
 
   toggleActive(status: boolean) {
     this.isActive = status;
+  }
+
+  async deleteResourceFromCollection() {
+    const result = await this.collectionService.deleteResourceFromCollection({
+      anchorCollectionId: this.collectionId,
+      resourceId: this.data._id,
+    });
+
+    console.log(result);
   }
 }
