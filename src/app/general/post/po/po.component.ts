1e6566a8089feaa37e5852971df9c4925772eb1b
Raj Saha <saha.raj@mobileone.com.my>
2020-09-25 12:08:08 +0800

    fixed edit resource component

--- src/app/general/post/po/po.component.ts
+++ src/app/general/post/po/po.component.ts
@@ -1,48 +1,67 @@
-import { Component, OnInit } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router, ActivatedRoute } from '@angular/router';
-import { faShare, faShareAlt, faLink, faBackspace } from '@fortawesome/free-solid-svg-icons';
-import { PopoverRef } from 'src/app/components/popover/popover-ref';
+import { Component, OnInit } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Router, ActivatedRoute } from "@angular/router";
+import {
+  faShare,
+  faShareAlt,
+  faLink,
+  faBackspace,
+  faEdit,
+} from "@fortawesome/free-solid-svg-icons";
+import { PopoverRef } from "src/app/components/popover/popover-ref";
 
 @Component({
-  selector: 'app-po',
-  templateUrl: './po.component.html',
-  styleUrls: ['./po.component.scss']
+  selector: "app-po",
+  templateUrl: "./po.component.html",
+  styleUrls: ["./po.component.scss"],
 })
 export class PoComponent implements OnInit {
   id: string;
   username: string;
+  loggedInUser: string;
 
   // Icons
   faShare = faShare;
   faShareAlt = faShareAlt;
   faLink = faLink;
   faBackspace = faBackspace;
+  faEdit = faEdit;
 
   constructor(
     private snackBar: MatSnackBar,
     private router: Router,
     private route: ActivatedRoute,
-    private popoverRef: PopoverRef) { }
+    private popoverRef: PopoverRef
+  ) {}
 
   ngOnInit() {
     this.id = this.popoverRef.data.resourceId;
     this.username = this.popoverRef.data.username;
+    this.loggedInUser = localStorage.getItem("username");
   }
 
   openSnackBar(message: string, action: string) {
     this.snackBar.open(message, action, {
-      duration: 2000
+      duration: 2000,
     });
   }
 
-  goToView() {
-    this.router.navigate([`/profile/user/${this.username}/resource/${this.id}`], { relativeTo: this.route.parent });
+  view() {
+    this.router.navigate(
+      [`/profile/user/${this.username}/resource/${this.id}`],
+      { relativeTo: this.route.parent }
+    );
+    this.close();
+  }
+
+  edit() {
+    this.router.navigate([`/manage/resource/edit/${this.id}`], {
+      relativeTo: this.route.parent,
+    });
     this.close();
   }
 
   close() {
     this.popoverRef.close();
   }
-
 }
